6e33250c860e94574f19579520c396d96bff56f3
goKrishGitspace <krishCloudspace@gmail.com>
2026-05-04 16:06:48 +0000

    products created on server.js saved

--- server.js
+++ server.js
@@ -54,6 +54,17 @@ app.use((req, res) => {
   });
 });
 
+app.get('/products', (req, res) => {
+  res.json(products);
+}); 
+
+let products=[
+  { id: 1, name: 'Product A', price: 10.99 },
+  { id: 2, name: 'Product B', price: 19.99 },
+  { id: 3, name: 'Product C', price: 5.49 },
+  { id: 4, name: 'Product D', price: 15.00 },
+]
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
